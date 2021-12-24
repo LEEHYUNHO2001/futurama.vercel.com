10e63c1a728a071b928ad0730ccfdcf0e5f98e71
HyunHo Lee <dlgusgh2001@naver.com>
2021-12-25 02:16:08 +0900

    ♻️ constants(상수 관리) - API_ENDPOINT 분리

--- src/hooks/useCharactorData.tsx
+++ src/hooks/useCharactorData.tsx
@@ -1,6 +1,7 @@
 import useSWR from "swr";
 import { fetcher } from "../utils/fetcher";
+import { API_ENDPOINT } from "../constants";
 
 export const useCharactorData = () => {
-  return useSWR("https://api.sampleapis.com/futurama/characters", fetcher);
+  return useSWR(`${API_ENDPOINT}/characters`, fetcher);
 };
