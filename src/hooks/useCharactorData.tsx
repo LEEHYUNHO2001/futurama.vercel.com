2a1198f68c3c29256f1d8e8ed31ba4b452214857
HyunHo Lee <dlgusgh2001@naver.com>
2021-12-25 02:12:20 +0900

    ♻️ utils - fetcher 분리

--- src/hooks/useCharactorData.tsx
+++ src/hooks/useCharactorData.tsx
@@ -1,7 +1,5 @@
 import useSWR from "swr";
-import axios from "axios";
-
-const fetcher = (url: string) => axios(url).then((res) => res.data);
+import { fetcher } from "../utils/fetcher";
 
 export const useCharactorData = () => {
   return useSWR("https://api.sampleapis.com/futurama/characters", fetcher);
