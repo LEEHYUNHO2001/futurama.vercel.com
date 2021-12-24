dcc13973e9034409c8cbbc0347f008d7ac90ec71
HyunHo Lee <dlgusgh2001@naver.com>
2021-12-25 01:39:53 +0900

    💄 CharactorsIndexPage style

--- src/pages/charactors/index.tsx
+++ src/pages/charactors/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import useSWR from "swr";
 import axios from "axios";
 import { CharactorData } from "../types/charactors";
+import styled from "@emotion/styled";
 
 const fetcher = (url: string) => axios(url).then((res) => res.data);
 
@@ -10,40 +11,53 @@ const CharactorsIndexPage: NextPage = () => {
     "https://api.sampleapis.com/futurama/characters",
     fetcher
   );
-  console.log(data);
   if (error) return <div>Error...</div>;
   if (!data) return <div>Loading...</div>;
 
   return (
     <div>
-      <main>
-        <h1>Charactors</h1>
-        <p>
-          {data.map((charactor: CharactorData) => {
-            const {
-              age,
-              gender,
-              homePlanet,
-              id,
-              species,
-              images,
-              name,
-              occupation,
-              saying,
-            } = charactor;
-            return (
-              <div key={`futurama-charactor-${id}`}>
-                <img src={images.main} alt={name.first} />
-                <h1>
-                  {name.first} {name.middle} {name.last}
-                </h1>
-              </div>
-            );
-          })}
-        </p>
-      </main>
+      <h1>Charactors</h1>
+      <Container>
+        {data.map((charactor: CharactorData) => {
+          const {
+            age,
+            gender,
+            homePlanet,
+            id,
+            species,
+            images,
+            name,
+            occupation,
+            saying,
+          } = charactor;
+          return (
+            <CharactorCard key={`futurama-charactor-${id}`}>
+              <Profile src={images.main} alt={name.first} />
+              <h1>
+                {name.first} {name.middle} {name.last}
+              </h1>
+            </CharactorCard>
+          );
+        })}
+      </Container>
     </div>
   );
 };
+
+//할 일 : 미디어 쿼리 추가하기
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`;
+const CharactorCard = styled.div`
+  display: flex;
+  max-width: 360px;
+`;
+const Profile = styled.img`
+  width: 100%;
+  aspect-ratio: 9 / 16;
+  object-fit: contain;
+`;
 
 export default CharactorsIndexPage;
