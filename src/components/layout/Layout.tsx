b3d98f1e5e456db6ce0da53c36d0b901d558c7c4
HyunHo Lee <dlgusgh2001@naver.com>
2021-12-28 09:32:11 +0900

    ♻️ Layout - Mobile Nav

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,78 +1,17 @@
 import { Navigation } from "./Navigation";
 import styled from "@emotion/styled";
-import { MEDIA_QUERY_END_POINT } from "../../constants";
-import { useState } from "react";
-import Link from "next/link";
+import { MobileNav } from ".";
 
 export const Layout: React.FC = ({ children }) => {
-  const [openMenu, setOpenMenu] = useState(false);
-
-  const openMenuMobile = () => {
-    setOpenMenu(!openMenu);
-  };
-
   return (
     <Container>
-      <Toggle onClick={openMenuMobile}>
-        <H1>MENU</H1>
-        {openMenu && (
-          <Nav>
-            <Link href="/">
-              <a>HOME</a>
-            </Link>
-            <Link href="/info">
-              <a>INFO</a>
-            </Link>
-            <Link href="/characters">
-              <a>CHARACTERS</a>
-            </Link>
-            <Link href="/cast">
-              <a>CAST</a>
-            </Link>
-            <Link href="/episodes">
-              <a>EPISODES</a>
-            </Link>
-            <Link href="/questions">
-              <a>QUESTIONS</a>
-            </Link>
-            <Link href="/inventory">
-              <a>INVENTORY</a>
-            </Link>
-          </Nav>
-        )}
-      </Toggle>
+      <MobileNav />
       <Navigation />
       <Main>{children}</Main>
     </Container>
   );
 };
 
-const Toggle = styled.div`
-  display: none;
-  position: fixed;
-  right: 0;
-  margin: 20px 20px;
-  cursor: pointer;
-  @media (max-width: ${MEDIA_QUERY_END_POINT.MOBILE}) {
-    display: inline-block;
-  }
-`;
-const H1 = styled.h1`
-  display: inline-block;
-  font-size: 18px;
-  font-weight: bold;
-  text-shadow: -1px 0 #fff, 0 1px #00fff0, 1px 0 #fff, 0 -1px #fff;
-  background-color: #000;
-  color: #fff;
-  padding: 5px 10px;
-  border-radius: 20px;
-`;
-const Nav = styled.nav`
-  display: flex;
-  flex-direction: column;
-  font-size: 12px;
-  font-weight: bold;
-`;
 const Container = styled.body`
   background-color: #05aff2;
   /* overflow-x: hidden; */
---
+++ src/components/layout/MobileNav.tsx
@@ -0,0 +1,69 @@
+import styled from "@emotion/styled";
+import { MEDIA_QUERY_END_POINT } from "../../constants";
+import Link from "next/link";
+import { useState } from "react";
+
+export const MobileNav = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const openMenuMobile = () => {
+    setOpenMenu(!openMenu);
+  };
+  return (
+    <Toggle onClick={openMenuMobile}>
+      <H1>MENU</H1>
+      {openMenu && (
+        <Nav>
+          <Link href="/">
+            <a>HOME</a>
+          </Link>
+          <Link href="/info">
+            <a>INFO</a>
+          </Link>
+          <Link href="/characters">
+            <a>CHARACTERS</a>
+          </Link>
+          <Link href="/cast">
+            <a>CAST</a>
+          </Link>
+          <Link href="/episodes">
+            <a>EPISODES</a>
+          </Link>
+          <Link href="/questions">
+            <a>QUESTIONS</a>
+          </Link>
+          <Link href="/inventory">
+            <a>INVENTORY</a>
+          </Link>
+        </Nav>
+      )}
+    </Toggle>
+  );
+};
+
+const Toggle = styled.div`
+  display: none;
+  position: fixed;
+  right: 0;
+  margin: 20px 20px;
+  cursor: pointer;
+  @media (max-width: ${MEDIA_QUERY_END_POINT.MOBILE}) {
+    display: inline-block;
+  }
+`;
+const H1 = styled.h1`
+  display: inline-block;
+  font-size: 18px;
+  font-weight: bold;
+  text-shadow: -1px 0 #fff, 0 1px #00fff0, 1px 0 #fff, 0 -1px #fff;
+  background-color: #000;
+  color: #fff;
+  padding: 5px 10px;
+  border-radius: 20px;
+`;
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  font-size: 12px;
+  font-weight: bold;
+`;
